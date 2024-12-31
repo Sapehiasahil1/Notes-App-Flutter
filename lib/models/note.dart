825c7c2f242b7ebbbd8ddebc4a1ea18aea3d23f0
Sapehiasahil1 <officialsahilsapehia03@gmail.com>
2024-12-31 15:08:01 +0530

    Database Created

---
+++ lib/models/note.dart
@@ -0,0 +1,34 @@
+class Note {
+  int? id;
+  String title;
+  String content;
+  String noteColor;
+
+  Note(
+      {this.id,
+      this.title = "Note",
+      this.content = "Text",
+      this.noteColor = "Red"
+      });
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> data = Map<String, dynamic>();
+    if(id != null)
+      data['id'] = id;
+
+    data['title'] = title;
+    data['content'] = content;
+    data['noteColor'] = noteColor;
+    return data;
+}
+
+@override
+  String toString() {
+    return {
+      'id': id,
+      'title':title,
+      'content':content,
+      'noteColor': noteColor,
+    }.toString();
+  }
+}
